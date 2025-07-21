b8639ed9368e4defe630e7d96280f2feab768e16
pnigam07 <in.pankajnigam@gmail.com>
2025-07-21 14:34:53 -0400

    Initial Commit

---
+++ PKN/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PKN
+//
+//  Created by pankaj nigam on 7/21/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PKN/PKNApp.swift
@@ -0,0 +1,17 @@
+//
+//  PKNApp.swift
+//  PKN
+//
+//  Created by pankaj nigam on 7/21/25.
+//
+
+import SwiftUI
+
+@main
+struct PKNApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
